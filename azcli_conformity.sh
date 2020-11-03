bebd3fcb2ad36ca499c2911a99865d2830890c5e
Felipe Costa <felipe.costa09@outlook.com>
2020-11-03 07:02:45 +0400

    Implement requests form the last PR (#9)
    * Deploy improved code
    
    * Update README with better overview of the tool
    
    * Implement last request from the old PR
    
    * Fix first conflict
    
    * Fixing multiple subscription and unique Role (#3)
    
    * Fixing issues with multiple subscriptions and Cloud Shell
    
    * Revert "Fixing issues with multiple subscriptions and Cloud Shell"
    
    This reverts commit fa41c69b8953ca4b4570748294f40dfc6f914821.
    
    * Update support to multiple subscription
    
    Improve to use a single Custom Role
    
    Co-authored-by: Fernando Cardoso (Kalopa) <fernando0stc@gmail.com>

--- azcli_conformity.sh
+++ azcli_conformity.sh
@@ -1,5 +1,18 @@
-# Define Variables for the Configurations
+#!/bin/bash
+
+# Fail the script in case any of the commands get an error
+set -e 
+
+# Define Variables for the Configurations using uuidgen or request an Application Secret if you are using Azure Cloud Shell
 export APP_SECRET=$(uuidgen)
+if [ -z $APP_SECRET ]
+then
+   echo "Please enter with the APPLICATION SECRET to be used by Conformity:"
+   IFS=$'\n'
+   read -r APP_SECRET
+fi
+export CUSTOM_ROLE="Custom Role - Cloud One Conformity"
+
 
 # Ask User Input for the App Registration Name
 echo " "
@@ -10,30 +23,43 @@ echo " "
 echo "Hello, Thank you for trying this script out.
 We're configuring your Azure Account to work with Cloud One Conformity, this might take several minutes depending of how many subscriptions you have."
 
-# Create App Registration
-az ad app create --display-name "$APP_NAME" --password $APP_SECRET --credential-description secret --required-resource-accesses @manifest.json > /dev/null
+# Create App Registration and assign MSGraph and AADGraph permissions required as part of the application registration process
+az ad app create \
+   --display-name "$APP_NAME" \
+   --password $APP_SECRET \
+   --credential-description cocsecret \
+   --required-resource-accesses @manifest.json > /dev/null
 
-# Export App Registration ID and Active Directory ID
+# Export App Registration ID and Active Directory ID to variables
 export AD_ID=`az account show --query tenantId --output tsv`
-export APPID=`az ad app list --display-name "$APP_NAME" | grep appId | cut -d ":" -f2 | grep -o '".*"' | sed 's/^"\(.*\)".*/\1/'`
+export APP_ID=`az ad app list --display-name "$APP_NAME" | grep appId | cut -d ":" -f2 | grep -o '".*"' | sed 's/^"\(.*\)".*/\1/'`
 
 # Create a Service Principal for the App Registration
-az ad sp create --id $APPID > /dev/null
+az ad sp create --id $APP_ID > /dev/null
 
-# Grant Admin Consent to application
-az ad app permission admin-consent --id $APPID
+# Granting tenant-wide admin consent to the App Registration
+az ad app permission admin-consent --id $APP_ID
 
 # Export All Subscriptions ID's to an Array and Count to a Variable
 az account list --refresh > /dev/null
 export SUBSCRIPTIONS=(`az account list | grep id | cut -d ":" -f2 | grep -o '".*"' | sed 's/^"\(.*\)".*/\1/'`)
-NUM_SUBS=${#SUBSCRIPTIONS[@]}
+export NUM_SUBS=${#SUBSCRIPTIONS[@]}
 
- # Loop to add each one of the Subscriptions
- i=0
- for (( i; i<=$NUM_SUBS; i++ ))
- do  
-    az role definition create --role-definition '{ \
-      "Name": "Custom Role - Cloud One Conformity", \
+# Loop to add each one of the Subscriptions to assigns the Reader role and Custom role required by Conformity
+i=0
+for (( i; i<$NUM_SUBS; i++ ))
+do
+   if [ $i -eq 0 ]
+   then
+      AssingScopes="\"/subscriptions/${SUBSCRIPTIONS[i]}\""
+   else
+      AssingScopes+=", \"/subscriptions/${SUBSCRIPTIONS[i]}\""
+   fi
+done
+
+az role definition create \
+    --role-definition '{ \
+      "Name": "'$CUSTOM_ROLE'", \
       "Description": "Subscription level custom role for Cloud Conformity access.", \
       "Actions": [ \
          "Microsoft.AppConfiguration/configurationStores/ListKeyValue/action", \
@@ -43,18 +69,28 @@ NUM_SUBS=${#SUBSCRIPTIONS[@]}
       ], \
       "DataActions": [], \
       "NotDataActions": [], \
-      "AssignableScopes": ["/subscriptions/'${SUBSCRIPTIONS[i]}'"] \
+      "AssignableScopes":['$AssingScopes'] \
       }' > /dev/null
-   sleep 5
-   az role assignment create --role "Custom Role - Cloud One Conformity" --assignee $APPID --scope /subscriptions/${SUBSCRIPTIONS[i]} > /dev/null
-   sleep 5
-   az role assignment create --role Reader --assignee $APPID --scope /subscriptions/${SUBSCRIPTIONS[i]} > /dev/null
- done
+
+i=0
+for (( i; i<$NUM_SUBS; i++ ))
+do  
+   sleep 30
+   az role assignment create \
+      --assignee $APP_ID \
+      --role "$CUSTOM_ROLE" \
+      --scope /subscriptions/${SUBSCRIPTIONS[i]} > /dev/null
+   sleep 30
+   az role assignment create \
+      --role Reader \
+      --assignee $APP_ID \
+      --scope /subscriptions/${SUBSCRIPTIONS[i]} > /dev/null
+done
 
 # Print All the Information
-echo "Here the information that you'll need to use to finish the integration"
+echo "Here is the information that you'll need to use to finish the integration"
 echo " "
 echo 'Active Directory ID: '$AD_ID
-echo 'Application ID: '$APPID
+echo 'Application ID: '$APP_ID
 echo 'Application Secret: '$APP_SECRET
 echo " "
